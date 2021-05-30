9f7471979e34a4d08ee112f57d2c30cbccc164ee
Gursimran Kaur Kang <kang7@sheridancollege.ca>
2021-05-29 20:13:42 -0400

    Create the empty project

---
+++ src/test/java/sheridan/kang7/exercise1v1/Exercise1v1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.kang7.exercise1v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1v1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
